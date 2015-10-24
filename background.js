deceddc8c6a90bdc1c195831af646a0fa9b58495
Anil Shanbhag <anil@Alchemist.local>
2015-10-24 15:59:33 -0400

    adding an options page

--- background.js
+++ background.js
@@ -1,4 +1,19 @@
-chrome.webNavigation.onBeforeNavigate.addListener(function(details){
-    if(details.url.indexOf("twitch.tv") > -1)
-        chrome.tabs.update(details.tabId, {url:"https://www.youtube.com/watch?v=yX39J_YyKbs"});
+chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
+  chrome.storage.sync.get({
+    patterns: '',
+    redirectLink: 'https://www.youtube.com/watch?v=yX39J_YyKbs'
+  }, function(items) {
+    var parts = items.patterns.split('\n');
+    var redirect = false;
+    for (var i=0; i<parts.length; i++) {
+      var pattern = parts[i].trim();
+      if (pattern && details.url.indexOf(pattern) > -1) {
+        redirect = true;
+        break;
+      }
+    }
+
+    if (redirect)
+      chrome.tabs.update(details.tabId, {url : items.redirectLink});
+  });
 });
---
+++ options.js
@@ -0,0 +1,31 @@
+function save_options() {
+  var patterns = document.querySelector('#patterns').value;
+  var redirectLink = document.querySelector('#redirectLink').value;
+  // TODO: Add validation
+  chrome.storage.sync.set({
+    patterns: patterns,
+    redirectLink: redirectLink
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.innerHTML = 'Options saved.';
+    setTimeout(function() {
+      status.innerHTML = '';
+    }, 2000);
+  });
+  return false;
+}
+
+function restore_options() {
+  chrome.storage.sync.get({
+    patterns: '',
+    redirectLink: 'https://www.youtube.com/watch?v=yX39J_YyKbs'
+  }, function(items) {
+    document.getElementById('patterns').value = items.patterns;
+    document.getElementById('redirectLink').value = items.redirectLink;
+  });
+}
+
+var form = document.querySelector('#submit');
+form.addEventListener('click', save_options);
+document.addEventListener('DOMContentLoaded', restore_options);
